492d856f4412cdd15918f518edd87d6aa371f4a2
Tanu-Khosla <55251550+Tanu-Khosla@users.noreply.github.com>
2019-10-02 01:42:17 -0400

    Class TriangleSolver

---
+++ TriangleSolver.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment
+{
+   public  class TriangleSolver
+    {
+        public static string Analyze(int x, int y, int z)
+        {
+            if (x + y <= z || x + z <= y || y + z <= x)
+            {
+                return "It is not a valid triangle";
+            }
+            else
+            {
+                if (x == y && y == z)
+                {
+                    return "This is an equilateral triangle";
+                }
+                else if (x == y || x == z || y == z)
+                {
+                    return "This is an isosceles triangle";
+                }
+                else
+                {
+                    return "This is a scalene triangle";
+                }
+               // return "It is a valid";
+            }
+        }
+    }
+}
